636b7345189b0bb33061c3f8b0c1bec8fcb5a5f1
tuongtran23 <63479988+tuongtran23@users.noreply.github.com>
2021-12-13 23:39:17 -0500

    finalized work

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,38 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="Location name is required.")
+    @Size(min=2,max = 200, message="Location name must be between 2 and 200 characters.")
+    public String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer()
+    {}
+
+    public Employer(String location){
+        super();
+        this.location = location;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
 
 }
